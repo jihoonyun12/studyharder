b00985833da29f94ec08e468568a43ef99cb08b1
윤지훈 <yunjihoon@yunjihun-ui-MacBookPro.local>
2020-04-14 17:04:02 +0900

    homework1

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yunjihoon/Desktop/멋쟁이사자처럼/word_counter/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
